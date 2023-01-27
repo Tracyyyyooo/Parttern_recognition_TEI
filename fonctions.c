a95cd9ace6155da6825c125bacc112e902448cca
Tracyyyyooo <118213709+Tracyyyyooo@users.noreply.github.com>
2023-01-27 11:35:02 +0100

    Add files via upload

---
+++ fonctions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pngwrap.h"
+#include <stdlib.h>
+
+int cherchermax(float *data)
+{
+    int position;
+    int n=sizeof(data)/sizeof(data[0]);
+    float max=data[1];
+    for (int i = 0;i<n;i++)
+    {
+            if(data[i]>max){
+                max=data[i];
+                position = i;
+            }
+            else;
+        }
+        return position;
+}
+
