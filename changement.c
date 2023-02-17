392574cb17c6223b26c6f3c8a8c6fad82588c3ae
Tracyyyyooo <118213709+Tracyyyyooo@users.noreply.github.com>
2023-02-17 10:46:43 +0100

    Update changement.c

--- changement.c
+++ changement.c
@@ -1,31 +1,3 @@
-//imComplex2Reel (et normalise entre 0 et 255)
-bwimage_t* imComplex2Reel(image_c *imc)
-{
-    bwimage_t  *im = malloc(sizeof(bwimage_t));
-    im->width = imc->width;
-    im->height = imc->height;
-    im->rawdata=malloc(im->width*im->height*sizeof(unsigned long));
-    float max=imc->rawdata[cherchermax(*imc)].re;
-    float min=imc->rawdata[cherchermin(*imc)].re;
-    int j=0, k=0;
-    for (int i=0; i<imc->width*imc->height;i++){;
-        im->(rawdata[i]=imc->rawdata[i].re-min)/(max-min)*255;
-    }
-    /*
-    //on normalise les valeurs entre 0 et 255
-    float max=im->rawdata[cherchermax(*im)];
-    float min=im->rawdata[cherchermin(*im)];
-    float a=255/(max-min);
-    for (int i=0;i<im->height*im->width;i++)
-    {
-        im->rawdata[i]=a*(im->rawdata[i]-min);
-    }*/
-    data(im);
-    return im;
-    
-}
-
-
 int cherchermax(image_c *imc){
     int position=0;
     int n=imc->width*imc->height;
@@ -33,7 +5,7 @@ int cherchermax(image_c *imc){
     for (int i = 0;i<n;i++)
     {
             if(imc->rawdata[i].re>max){
-                max=im->rawdata[i].re;
+                max=imc->rawdata[i].re;
                 position = i;
             }
             else;
@@ -54,4 +26,29 @@ int cherchermin(image_c *imc){
             else;
         }
         return position;
+}
+
+bwimage_t* imComplex2Reel(image_c *imc)
+{
+    bwimage_t  *im = malloc(sizeof(bwimage_t));
+    im->width = imc->width;
+    im->height = imc->height;
+    im->rawdata=malloc(im->width*im->height*sizeof(unsigned long));
+    float max=imc->rawdata[cherchermax(imc)].re;
+    float min=imc->rawdata[cherchermin(imc)].re;
+    for (int i=0; i<imc->width*imc->height;i++){;
+        im->rawdata[i]=(imc->rawdata[i].re-min)/(max-min)*255;
+    }
+    /*
+    //on normalise les valeurs entre 0 et 255
+    float max=im->rawdata[cherchermax(*im)];
+    float min=im->rawdata[cherchermin(*im)];
+    float a=255/(max-min);
+    for (int i=0;i<im->height*im->width;i++)
+    {
+        im->rawdata[i]=a*(im->rawdata[i]-min);
+    }*/
+    data(im);
+    return im;
+    
 }
